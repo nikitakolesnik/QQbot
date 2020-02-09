ef1781f7e3f3933c6ef983ebfb61461dcd357236
Nikita Kolesnik <nikitakolesnik@yahoo.com>
2020-02-09 13:10:11 -0800

    get player info by discord id

--- QQbot.Api/Services/Interfaces/IMatchRepository.cs
+++ QQbot.Api/Services/Interfaces/IMatchRepository.cs
@@ -6,10 +6,10 @@ namespace QQbot.Api.Services.Interfaces
 {
 	public interface IMatchRepository
 	{
-		Task<IEnumerable<Player>> GetPlayerInfoAsync(string[] names);
-		Task<IEnumerable<Player>> GetPlayerInfoAsync(string   nameCommaString);
-		Task<IEnumerable<Player>> GetPlayerInfoAsync(int[]    playerIds);
-		Task<IEnumerable<Player>> GetPlayerInfoAsync(long[]   discordIds);
+		Task<IEnumerable<Player>> GetPlayerInfoAsync(string[] names);           // ["slam", "yoko", "candy", ...]
+		Task<IEnumerable<Player>> GetPlayerInfoAsync(string   nameCommaString); // "slam,yoko,candy,..."
+		Task<IEnumerable<Player>> GetPlayerInfoAsync(int[]    playerIds);       // [1, 2, 3, ...]
+		Task<IEnumerable<Player>> GetPlayerInfoAsync(long[]   discordIds);      // [240413827718578177, 175325337196953600, 287275232236929026, ...]
 		Task<int> GetMaxRatingDifferenceAsync();
 		Task<int> RecordMatchAsync(IEnumerable<Player> winningTeam, IEnumerable<Player> losingTeam);
 	}
